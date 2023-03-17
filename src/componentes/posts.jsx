5d4a309729944a28fc8f72da6fe4a749340572fa
katarem <crismigesp@gmail.com>
2023-03-17 02:54:15 +0000

    cambio a ver que tal

--- src/componentes/posts.jsx
+++ src/componentes/posts.jsx
@@ -14,10 +14,10 @@ class Posts extends React.Component {
           porfolio donde iré incluyendo mi trabajo por la red.
         </p>
         <h2>PROYECTOS</h2>
-        <Post text="Katapi" link="https://github.com/katarem/KatApi" />
-        <Post text="Lolstats" link="https://github.com/dam-dad/LoLstats" />
+        <Post text="Katapi  Librería para usar API de Riot en Java" link="https://github.com/katarem/KatApi" />
+        <Post text="Lolstats  Visualizador de estadísticas en JavaFX" link="https://github.com/dam-dad/LoLstats" />
         <Post
-          text="BasicWeatherApp"
+          text="BasicWeatherApp  Buscador de temperatura y clima en ciudades de España en js"
           link="https://github.com/katarem/BasicWeatherApp"
         />
       </div>
