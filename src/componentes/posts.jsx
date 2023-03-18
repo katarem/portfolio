0f6f1d3ed98d9a21a0cf6b9e6627a8389ec406be
katarem <crismigesp@gmail.com>
2023-03-18 15:31:34 +0000

    changes

--- src/componentes/posts.jsx
+++ src/componentes/posts.jsx
@@ -13,13 +13,19 @@ class Posts extends React.Component {
           Hola, soy Cristian, desarrollador multiplataforma y este es mi
           porfolio donde iré incluyendo mi trabajo por la red.
         </p>
+        <h2>SKILLS</h2>
+        {/* <p>JAVA, JAVAFX, SQL, JS, React, Git</p> */}
         <h2>PROYECTOS</h2>
-        <Post text="Katapi  Librería para usar API de Riot en Java" link="https://github.com/katarem/KatApi" />
-        <Post text="Lolstats  Visualizador de estadísticas en JavaFX" link="https://github.com/dam-dad/LoLstats" />
+        <Post text="Katapi  Librería API RiotGames" link="https://github.com/katarem/KatApi" />
+        <Post text="Lolstats  StatChecker de LoL JavaFX" link="https://github.com/dam-dad/LoLstats" />
         <Post
-          text="BasicWeatherApp  Buscador de temperatura y clima en ciudades de España en js"
+          text="BasicWeatherApp  Clima de España en js"
           link="https://github.com/katarem/BasicWeatherApp"
         />
+        
+        <div>
+          <img></img>
+        </div>
       </div>
     );
   }
