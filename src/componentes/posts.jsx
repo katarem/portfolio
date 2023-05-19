c787abf7606f239b8209d44aafb4583bb74fb969
katarem <crismigesp@gmail.com>
2023-05-19 02:51:43 +0200

    commit inspiracion a las 2am

--- src/componentes/posts.jsx
+++ src/componentes/posts.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Post from "./post";
+
 class Posts extends React.Component {
   constructor() {
     super();
@@ -14,15 +15,53 @@ class Posts extends React.Component {
           porfolio donde iré incluyendo mi trabajo por la red.
         </p>
         <h2>SKILLS</h2>
-        {/* <p>JAVA, JAVAFX, SQL, JS, React, Git</p> */}
+        <div className="skillBox">
+          <img
+            src="https://classes.engineering.wustl.edu/cse231/core/images/2/26/Java.png"
+            className="linkButton"
+          ></img>
+          <img
+            src="https://cdn.cdnlogo.com/logos/c/68/c-sharp-800x800.png"
+            className="linkButton"
+          ></img>
+          <img
+            src="https://static.wixstatic.com/media/2724b2_a4c660815dde4271be00cb7e9b9cae2c~mv2.png/v1/fit/w_605%2Ch_533%2Cal_c%2Cq_80,enc_auto/file.jpg"
+            className="linkButton"
+          ></img>
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/4492/4492311.png"
+            className="linkButton"
+          ></img>
+          <img
+            src="https://1000logos.net/wp-content/uploads/2020/08/MySQL-Logo.png"
+            className="linkButton"
+          ></img>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"
+            className="linkButton"
+          ></img>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png"
+            className="linkButton"
+          ></img>
+        </div>
+
+        {/* Idea de las 3 am polacas, cada post, es un desplegable, con un slider de 3 imágenes y un resumen del proyecto */}
+
         <h2>PROYECTOS</h2>
-        <Post text="Katapi  Librería API RiotGames" link="https://github.com/katarem/KatApi" />
-        <Post text="Lolstats  StatChecker de LoL JavaFX" link="https://github.com/dam-dad/LoLstats" />
         <Post
-          text="BasicWeatherApp  Clima de España en js"
-          link="https://github.com/katarem/BasicWeatherApp"
+          text="Katapi  Librería API RiotGames"
+          link="https://github.com/katarem/KatApi"
         />
-        
+        <Post
+          text="Lolstats  StatChecker de LoL JavaFX"
+          link="https://github.com/dam-dad/LoLstats"
+        />
+        <Post
+          text="RemBot  Bot de Discord en JavaScript"
+          link="https://github.com/katarem/portfolio"
+        />
+
         <div>
           <img></img>
         </div>
