d7106110d7b155eecccfedd21d3243bfb69d4dfe
katarem <crismigesp@gmail.com>
2024-03-16 22:10:57 +0000

    U Tecnologías parametrizadas

--- src/components/SkillsCard.tsx
+++ src/components/SkillsCard.tsx
@@ -1,15 +1,23 @@
-import { ReactNode } from 'react'
 import './SkillsCard.css'
+import tecnologiesJSON from '../assets/data/technologies.json'
+import LinkButton from './LinkButton'
 
-export default function SkillsCard({ lang,children }: {lang: Language,children: ReactNode}){
+interface Skill{
+    name: string,
+    url: string,
+    src: string
+}
 
+
+export default function SkillsCard({ lang }: {lang: Language}){
+    let technologies: Skill[] = tecnologiesJSON
     return(
         <section className='skills-root' id='skills'>
             <section className='skills-container'>
                 <h1>Skills</h1>
                 <p>{lang.skills}</p>
                 <div className='skills-grid'>
-                    {children}
+                    {technologies.map(tec => <LinkButton name={tec.name} src={tec.src} url={tec.url}/>)}
                 </div>
             </section>
         </section>
